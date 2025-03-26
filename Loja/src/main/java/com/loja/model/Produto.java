16538fcf5d07b2908fba2cc10cd361ec6025392c
otaviocode <luizgomesdossantos1970@gmail.com>
2025-03-26 13:25:41 -0300

    create class produto

---
+++ Loja/src/main/java/com/loja/model/Produto.java
@@ -0,0 +1,180 @@
+package com.loja.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	private Long id;
+	private String tipoUnidade;
+	private String nome;
+
+	@Column(columnDefinition = "text", length = 2000)
+	private String descricao;
+
+	private Double peso;
+	private Double largura;
+	private Double altura;
+	private Double produndidade;
+	private BigDecimal valorVenda = BigDecimal.ZERO;
+	private Integer QtdEstoque = 0;
+	private Integer QtdAlertaEstoque = 0;
+	private String linkYoutube;
+	private Boolean alertaQtdEstoque = Boolean.FALSE;
+	private Integer QtdClique = 0;
+	private Boolean ativo = Boolean.TRUE;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getProdundidade() {
+		return produndidade;
+	}
+
+	public void setProdundidade(Double produndidade) {
+		this.produndidade = produndidade;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Integer getQtdEstoque() {
+		return QtdEstoque;
+	}
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		QtdEstoque = qtdEstoque;
+	}
+
+	public Integer getQtdAlertaEstoque() {
+		return QtdAlertaEstoque;
+	}
+
+	public void setQtdAlertaEstoque(Integer qtdAlertaEstoque) {
+		QtdAlertaEstoque = qtdAlertaEstoque;
+	}
+
+	public String getLinkYoutube() {
+		return linkYoutube;
+	}
+
+	public void setLinkYoutube(String linkYoutube) {
+		this.linkYoutube = linkYoutube;
+	}
+
+	public Boolean getAlertaQtdEstoque() {
+		return alertaQtdEstoque;
+	}
+
+	public void setAlertaQtdEstoque(Boolean alertaQtdEstoque) {
+		this.alertaQtdEstoque = alertaQtdEstoque;
+	}
+
+	public Integer getQtdClique() {
+		return QtdClique;
+	}
+
+	public void setQtdClique(Integer qtdClique) {
+		QtdClique = qtdClique;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
