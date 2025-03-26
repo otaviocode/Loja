d4c806be32a84520296d6f96f1cf9165535dc624
otaviocode <luizgomesdossantos1970@gmail.com>
2025-03-26 13:35:02 -0300

    New configuration and new class imagem produto

--- Loja/src/main/java/com/loja/LojaApplication.java
+++ Loja/src/main/java/com/loja/LojaApplication.java
@@ -2,12 +2,13 @@ package com.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.loja.model")
 public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
-
 }
